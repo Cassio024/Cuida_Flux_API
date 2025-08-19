b60a70ecbba9fc500cfeebb51abc292aad47adab
Marco Túlio <gomesferreiramarcotulio@gmail.com>
2025-08-19 16:57:11 -0300

    Melhoria basica na notificação

--- utils/fcmService.js
+++ utils/fcmService.js
@@ -1,18 +1,26 @@
 const axios = require('axios');
 
 function enviarNotificacao(token, titulo, corpo) {
-  axios.post('https://fcm.googleapis.com/fcm/send', {
-    to: token,
-    notification: {
-      title: titulo,
-      body: corpo
+  axios.post(
+    'https://fcm.googleapis.com/fcm/send',
+    {
+      to: token,
+      notification: {
+        title: titulo,
+        body: corpo,
+        icon: '/icons/favicon.png',
+        badge: '/icons/favicon.png',
+        vibrate: [300, 200, 300, 200, 300]
+      },
+      data: { url: '/alarme' }
+    },
+    {
+      headers: {
+        Authorization: `key=${process.env.FCM_SERVER_KEY}`,
+        'Content-Type': 'application/json'
+      }
     }
-  }, {
-    headers: {
-      Authorization: `key=${process.env.FCM_SERVER_KEY}`,
-      'Content-Type': 'application/json'
-    }
-  }).catch(err => console.error('Erro ao enviar notificação:', err));
+  ).catch(err => console.error('Erro ao enviar notificação:', err));
 }
 
 module.exports = { enviarNotificacao };
--- utils/scheduler.js
+++ utils/scheduler.js
@@ -1,12 +1,15 @@
 const { enviarNotificacao } = require('./fcmService');
 
-function agendarAlarme(token, horario, titulo, corpo) {
-  const tempo = new Date(horario).getTime() - Date.now();
-  if (tempo <= 0) return;
-
-  setTimeout(() => {
-    enviarNotificacao(token, titulo, corpo);
-  }, tempo);
+function agendarAlarme(token, horarios, titulo, corpo) {
+  const lista = Array.isArray(horarios) ? horarios : [horarios];
+  lista.forEach(horario => {
+    const tempo = new Date(horario).getTime() - Date.now();
+    if (tempo > 0) {
+      setTimeout(() => {
+        enviarNotificacao(token, titulo, corpo);
+      }, tempo);
+    }
+  });
 }
 
 module.exports = { agendarAlarme };
